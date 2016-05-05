3accb09d4c71a36b85d6bd513cf2e9b77fb40791
duplyka.dmitry <dmitry.duplyka@techs.com.ua>
2016-05-05 21:44:18 +0300

    Fixed My account

--- wp-content/themes/on-sale/js/hi.js
+++ wp-content/themes/on-sale/js/hi.js
@@ -1,9 +1,11 @@
-jQuery('document').ready(function($){
-    var li = $("header ul#menu-main-menu>li");
+jQuery('document').ready(function ($) {
+    var li = $("header ul#menu-main-menu>li:last-child");
     var maina = li.children("a[href*='my-account']");
-    var newli = $("<li class='menu-item menu-item-type-custom menu-item-object-custom'></li>");
-    newli.append(maina.clone());
-    li.find('.sub-menu').prepend(newli);
-    maina.text("Привет, "+$('#thisusername').text());
-    $("header ul#menu-main-menu a[href*='logout']").attr("href",$('#thisuserlogout').text());
+    if (maina.length) {
+        var newli = $("<li class='menu-item menu-item-type-custom menu-item-object-custom'></li>");
+        newli.append(maina.clone());
+        li.find('.sub-menu').prepend(newli);
+        maina.text("Привет, " + $('#thisusername').text());
+        $("header ul#menu-main-menu a[href*='logout']").attr("href", $('#thisuserlogout').text());
+    }
 });
